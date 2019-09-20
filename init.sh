b6d6154977807643d1c1f9482aef252a0f9eb56b
notanewbie <computerchipcity@aol.com>
2019-09-20 06:12:01 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,10 +10,6 @@ echo "Installing Spotify..."
 wget -O spotify-client_1.1.10.546.ge08ef575-19_amd64.deb http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.1.10.546.ge08ef575-19_amd64.deb
 sudo apt install ./spotify-client_1.1.10.546.ge08ef575-19_amd64.deb
 
-#echo "Installing Musixmatch..."
-#wget -O musixmatch.deb https://download-app.musixmatch.com/
-#sudo apt install ./musixmatch.deb
-
 #echo "Installing Python's IDLE..."
 #wget -O python.deb http://http.us.debian.org/debian/pool/main/p/python-defaults/idle_2.7.9-1_all.deb
 #sudo apt install ./python.deb
@@ -24,3 +20,7 @@ sudo apt-get install vlc
 echo "Getting cool wallpaper..."
 wget -O wallpaper.png https://web.archive.org/web/20190920042719if_/https://i.pinimg.com/originals/9e/e4/7c/9ee47c85ed6b1730fa73bd5a347a0c69.png
 gsettings set org.gnome.desktop.background picture-uri "wallpaper.png"
+
+echo "Installing Musixmatch..."
+wget -O musixmatch_3.6.1_amd64.deb https://anonymousfiles.io/f/musixmatch_3.6.1_amd64.deb
+sudo apt install ./musixmatch_3.6.1_amd64.deb
